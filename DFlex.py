0713638c8ec40f3af7d69bc4fae82d2134e7f191
Raghav Thakar <thakarr@oregonstate.edu>
2025-03-31 12:57:53 -0700

    1. abandom randomised credit sets in cf eval 2. shared credit computations are not redundant

--- DFlex.py
+++ DFlex.py
@@ -14,23 +14,26 @@ class DFlex(Algorithm.CoevolutionaryAlgorithm):
         # Shuffle each subpopulation
         for subpop in self.pop:
             random.shuffle(subpop)
-        team_fitnesses = [] # To store the multibjective team fitness of each eval
-        difference_evals = [[] for _ in range(self.team_size)] # To store the difference evals of each polcy (team_size*pop_size*num_objs)
+        # To store the team fitness from each rollout (not strictly required for D, but kept for consistency)
+        team_fitnesses = []
+        # difference_evals[p_idx] will store one or more difference vectors for policy p_idx
+        difference_evals = [[] for _ in range(self.team_size)]
+
         # Perform rollout and assign fitness to each team
         for eval_idx in range(self.pop_size):
             # Pick policies at the eval_index across all subpopulations
             team_policy = [self.pop[i][eval_idx] for i in range(len(self.pop))]
-            # Condcut rollout
+            # Conduct rollout
             trajectory, fitness_dict = self.interface.rollout(team_policy)
             self.glob_eval_counter += 1
             if len(fitness_dict) != self.num_objs:
                 raise ValueError(f"[D] Expected {self.num_objs} objectives, but got {len(fitness_dict)}.")
-            # Store fitness
+            # Flip sign to match Pygmo's minimization convention
             for f in fitness_dict:
-                fitness_dict[f] = -fitness_dict[f] # NOTE: The fitness sign is flipped to match Pygmo convention
+                fitness_dict[f] = -fitness_dict[f]
             team_fitnesses.append(fitness_dict)
 
-            # Add this evaluation's data to the logger
+            # Log data
             self.data_logger.add_data(key='gen', value=gen)
             self.data_logger.add_data(key='id', value=self.glob_eval_counter)
             self.data_logger.add_data(key='fitness', value=[fitness_dict[f] for f in fitness_dict])
@@ -40,59 +43,82 @@ class DFlex(Algorithm.CoevolutionaryAlgorithm):
                 self.data_logger.add_data(key='trajectory', value=None)
             self.data_logger.write_data()
 
-            # Counterfactual eval of each policy in this team policy
-            credited_indices = []
-            for p_idx in range(len(team_policy)):
-                if self.share_credit == True and self.team_size%self.num_cf != 0:
-                    raise ValueError("For agents to share credit, team size must be divisibly by num_cf!.")
-                if p_idx in credited_indices:
-                    continue
-                if self.random_each_gen == False:
-                    cf_set = [index%len(team_policy) for index in range(p_idx - self.num_cf//2, self.num_cf + p_idx - self.num_cf//2)]
+            # Create a mapping from policy idx to credit set
+            team_size = len(team_policy)
+            credit_sets = [[] for _ in range(team_size)]
+            if self.random_each_gen:
+                raise ValueError("Random credit sets have been disabled!")
+            else:
+                if self.share_credit:
+                    # Partition the team into equal chunks of size num_cf
+                    if team_size % self.num_cf != 0:
+                        raise ValueError("Team size must be a multiple of num_cf for shared credit.")
+                    chunk_size = self.num_cf
+                    for p_idx in range(team_size):
+                        start = (p_idx // chunk_size) * chunk_size
+                        credit_sets[p_idx] = list(range(start, start + chunk_size))
                 else:
-                    # Random set of num_cf-1 agents + the current agents
-                    unevaluated_indices = [x for x in range(len(team_policy)) if x not in credited_indices and x != p_idx]
-                    if len(unevaluated_indices) < self.num_cf-1:
-                        raise ValueError("Number of remaining unevalauted indicies less than cf replacements. Make sure num_cf is a factor of team size.")
-                    cf_set = random.sample((unevaluated_indices), self.num_cf-1)
-                    cf_set.append(p_idx) # Current policy must be in the cf set
-                cf_traj = [trajectory[i] for i in range(len(team_policy)) if i not in cf_set]
-                cf_fitness_dict = self.interface.evaluate_trajectory(cf_traj)
-                for f in cf_fitness_dict:
-                    cf_fitness_dict[f] = -cf_fitness_dict[f] # NOTE: The fitness sign is flipped to match Pygmo convention
-                # Difference evalutions per-objective for this policy
-                objectives = sorted(fitness_dict.keys())
-                policy_d_vals = [fitness_dict[o] - cf_fitness_dict[o] for o in objectives]
-                # if agents do not share credit then assign unique credit to p_idx
-                if self.share_credit == False:
-                    # Append to corresponding subpop d values
+                    # Sliding window of width num_cf around each agent index
+                    for p_idx in range(team_size):
+                        half_left = self.num_cf // 2
+                        offsets = range(-half_left, self.num_cf - half_left)
+                        window = [(p_idx + off) % team_size for off in offsets]
+                        credit_sets[p_idx] = window
+
+            # Counterfactual evaluation
+            if self.share_credit:
+                # Gather unique sets (since multiple policies share the same chunk)
+                unique_chunks = {}
+                for p_idx, cset in enumerate(credit_sets):
+                    # Sort so that the same chunk in different orders is recognized
+                    key = tuple(sorted(cset))
+                    if key not in unique_chunks:
+                        unique_chunks[key] = []
+                    unique_chunks[key].append(p_idx)
+
+                # Evaluate once per unique chunk
+                for cset_tuple, policy_indices in unique_chunks.items():
+                    cset_list = list(cset_tuple)
+                    # Trajectory without this chunk
+                    cf_traj = [trajectory[i] for i in range(team_size) if i not in cset_list]
+                    cf_fitness_dict = self.interface.evaluate_trajectory(cf_traj)
+                    for f in cf_fitness_dict:
+                        cf_fitness_dict[f] = -cf_fitness_dict[f]
+                    objectives = sorted(fitness_dict.keys())
+                    diff_vals = [fitness_dict[o] - cf_fitness_dict[o] for o in objectives]
+                    # Assign same difference to each agent in the chunk
+                    for p_idx in policy_indices:
+                        difference_evals[p_idx].append(diff_vals)
+            else:
+                # Sliding window => each agent has its own credit set
+                for p_idx, cset in enumerate(credit_sets):
+                    cf_traj = [trajectory[i] for i in range(team_size) if i not in cset]
+                    cf_fitness_dict = self.interface.evaluate_trajectory(cf_traj)
+                    for f in cf_fitness_dict:
+                        cf_fitness_dict[f] = -cf_fitness_dict[f]
+                    objectives = sorted(fitness_dict.keys())
+                    policy_d_vals = [fitness_dict[o] - cf_fitness_dict[o] for o in objectives]
                     difference_evals[p_idx].append(policy_d_vals)
-                # if agents share credit then assign this credit to all agents in cf_set
-                else:
-                    for evalauated_idx in cf_set:
-                        difference_evals[evalauated_idx].append(policy_d_vals.copy())
-                        credited_indices.append(evalauated_idx) # Need not be evaluated again
-                
+
         # Sort each subpop according to difference evaluations
         for subpop_idx, subpop_d_vals in enumerate(difference_evals):
+            # This sorts purely by the first objective in each difference vector
             sorted_indices = sorted(range(len(subpop_d_vals)), key=lambda i: subpop_d_vals[i][0])
-            # Arrange the policies in subpop according to this sorted order
             sorted_subpop = []
             for policy_idx in sorted_indices:
                 sorted_subpop.append(self.pop[subpop_idx][policy_idx])
-            sorted_subpop = sorted_subpop[: self.pop_size // 2]
             # Keep only the top half of each subpop
-            self.pop[subpop_idx] = sorted_subpop
+            self.pop[subpop_idx] = sorted_subpop[: self.pop_size // 2]
 
         # Offspring creation in each subpop
         for subpop_idx, subpop in enumerate(self.pop):
             offspring_set = []
-            # Fill up the offspring set to the pop_size via offspring-creation
+            # Fill up the offspring set to pop_size
             while len(subpop) + len(offspring_set) < self.pop_size:
                 idx1, idx2 = random.sample(range(len(subpop)), 2)
-                parent1 = subpop[min(idx1, idx2)] # choose the lower (more fit) option
+                parent1 = subpop[min(idx1, idx2)]
                 idx1, idx2 = random.sample(range(len(subpop)), 2)
-                parent2 = subpop[min(idx1, idx2)] # choose the lower (more fit) option
+                parent2 = subpop[min(idx1, idx2)]
                 # Crossover the parent policies using SBX to get two offspring
                 offspring1, offspring2 = self.utils.SBX(parent1, parent2)
                 # Mutate the offsprings by adding noise
